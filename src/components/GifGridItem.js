30237d19e8256b37d64513082530711c6cd7b8f0
David Guzman <david.guzman@capitaltech.com>
2021-07-15 19:50:23 -0500

    add some animations to the app

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -4,7 +4,7 @@ export const GifGridItem = ({url, title, id}) => {
 
 
     return (
-        <div className='card'>
+        <div className='card animate__animated animate__fadeIn animate__delay-1s'>
             <img src={url} alt={title}></img>
             <p>{title}</p>
         </div>
